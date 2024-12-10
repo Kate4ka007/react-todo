f42d01da36afe54601ce6780529f25733df49698
Katsiaryna Shlemena <kate4ka0007@gmail.com>
2024-12-10 17:49:06 +0300

    fix: change config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-todo/",
   server: {
     host: '0.0.0.0',
     port: 3000,
