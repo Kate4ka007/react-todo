e0ee322d785ef91d5ea7d5a0704b4259d05bcbb8
Katsiaryna Shlemena <kate4ka0007@gmail.com>
2024-12-10 17:13:18 +0300

    feat: add build

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-
-import React from "react";
-
-import TodoApp from "./components/TodoApp/TodoApp.tsx";
-
-const App: React.FC = () => <TodoApp />;
-
-export default App;
--- src/utils/storage.ts
+++
@@ -1,8 +0,0 @@
-export const saveToStorage = <T>(key: string, data: T): void => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
-
-export const loadFromStorage = <T>(key: string): T | null => {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : null;
-};
